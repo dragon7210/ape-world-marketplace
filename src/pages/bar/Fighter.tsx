d5b3b2cbb673cf66d722a585d81201691fff55a7
dragon <firedragon7210@gmail.com>
2023-10-12 06:21:45 -0400

    bar initial

--- src/pages/bar/Fighter.tsx
+++ src/pages/bar/Fighter.tsx
@@ -3,7 +3,7 @@
 const Fighter = () => {
   return (
     <div className='lg:px-10 md:px-5 p-3 bg-[#00000050] min-h-[calc(100vh_-_180px)] md:min-h-[calc(100vh_-_300px)] rounded-xl'>
-      Fighter
+      Welcome to on-Chain Fighter Record
     </div>
   );
 };
